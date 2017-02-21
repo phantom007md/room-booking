50a8cbc5cc9ee96a5af66de6000713514a42398a
PHANTOM <mohamad.d007@gmail.com>
2017-02-21 07:48:24 +0330

    adding and removing roomIndex rows are done by managing the DOM and working with nodes.

--- src/modules/room.ts
+++ src/modules/room.ts
@@ -2,52 +2,77 @@
 
 namespace Booking {
   export class RoomManage {
-    private roomTemplate: string;
+    private roomTemplate: Element;
     private roomCount: number;
     private rooms: Object;
-    private roomSelectBox: Element;
     private childAgeTemplate: string;
     private infantAgeTemplate: string;
+    private roomIndex: number;
+    private roomIndexArr: Element[];
+    private rowIndexNum: number;
     constructor() {
-      this.bindEvents();
       this.roomCountVal();
-      this.templateGenrator();
+      this.bindDeligateEvents();
       this.templateInjector();
     }
     private roomCountVal(){
-      this.roomCount = Booking.$.selectBoxVal('.room-select');
+      let roomSelectValue = Booking.$.selectBoxVal('.room-select');
+      this.roomCount = parseInt(roomSelectValue);
     }
-    private bindEvents(){
-      this.roomSelectBox = Booking.$.select('.room-select');
+    private roomIndexCounter(){
+      this.roomIndexArr = Booking.$.selectall('.row-id');
+      this.roomIndex =  this.roomIndexArr.length
+    }
+    private rowIndexNumGenerate(){
+      let rowIds = Booking.$.selectall('.row-id');
+      this.rowIndexNum = rowIds.length + 1;
+    }
+    private bindDeligateEvents(){
+      Booking.$.on('body','change','.room-select', () => {
+        this.roomCountVal();
+        this.roomIndexCounter();
+        this.roomIndexManager();
+      });
+      // Booking.$.on('body','change','.child-count', () => {
+      //   this.childCountVal();
+      //   this.roomIndexCounter();
+      //   this.roomIndexManager();
+      // });
+    }
+    private roomIndexManager(){
+      if (this.roomCount == this.roomIndex) {
+        return
+      }else if(this.roomCount > this.roomIndex){
+        let nRoom = this.roomCount - this.roomIndex
+        for (let i = 0; i < nRoom; i++) {
+            this.templateInjector();
+        }
+      }else if(this.roomCount < this.roomIndex) {
+        this.removeRoomIndex();
+      }
+    }
+    private removeRoomIndex(){
+      let rowIds = Booking.$.selectall('.row-index');
+      let rowIdsNum = rowIds.length;
+      let needToDelete =  rowIdsNum - this.roomCount;
+      for (let i = 0; i < needToDelete; i++) {
+          rowIds[i+this.roomCount].remove();
+      }
     }
     private templateInjector(){
-      Booking.$.select('.row-index-wrap').innerHTML=this.roomTemplate;
+      this.rowIndexNumGenerate();
+      this.templateGenrator();
+      Booking.$.select('.row-index-wrap').appendChild(this.roomTemplate);
     }
     private templateGenrator() {
-      this.childAgeTemplate=`
-      <div class="child-age-wrap"><span>child </span><span>1</span><span>age</span>
-        <select class="child-age">
-          <option>2 YEAR</option>
-          <option>3 YEAR</option>
-          <option>4 YEAR</option>
-          <option>5 YEAR</option>
-          <option>6 YEAR</option>
-          <option>7 YEAR</option>
-        </select>
-      </div>`;
-      this.infantAgeTemplate=`
-      <div class="infant-age-wrap"><span>infant </span><span>1</span><span>age</span>
-        <select class="infant-age">
-          <option>0 YEAR </option>
-          <option>1 YEAR</option>
-          <option>2 YEAR</option>
-        </select>
-      </div>`;
-      this.roomTemplate = `
-        <div class="row-index"><span class="row-id">1</span>
+      let appendHandler = document.createElement('div');
+      appendHandler.classList.add('row-index')
+      let roomTemplateString = `
+          <span class="row-id">${this.rowIndexNum}</span>
           <div class="row">
             <div class="count-select"><span>adult count:</span>
               <select class="adult-count">
+                <option>0</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -65,17 +90,33 @@ namespace Booking {
               </select>
             </div>
             <div class="age-select">
-              ${this.childAgeTemplate}
-              ${this.infantAgeTemplate}
             </div>
-          </div>
-        </div>`;
+          </div>`;
+        appendHandler.innerHTML = roomTemplateString;
+        this.roomTemplate = appendHandler;
     }
-
-
-
-
-
+    private childTemplateGenerator(){
+      this.childAgeTemplate=`
+      <div class="child-age-wrap"><span>child </span><span>1</span><span>age</span>
+        <select class="child-age">
+          <option>2 YEAR</option>
+          <option>3 YEAR</option>
+          <option>4 YEAR</option>
+          <option>5 YEAR</option>
+          <option>6 YEAR</option>
+          <option>7 YEAR</option>
+        </select>
+      </div>`;
+    }
+    private infantTemplateGenerator(){
+      this.infantAgeTemplate=`
+      <div class="infant-age-wrap"><span>infant </span><span>1</span><span>age</span>
+        <select class="infant-age">
+          <option>0 YEAR </option>
+          <option>1 YEAR</option>
+          <option>2 YEAR</option>
+        </select>
+      </div>`;
+    }
   }
-
 }
--- src/modules/util.ts
+++ src/modules/util.ts
@@ -1,32 +1,66 @@
 namespace Booking {
   export class $ {
-
+/**
+ * select the element from a custom wrapper or document wraper bye a query selector.
+ * @param  {Node} s         The element we want to select.
+ * @param  {Node} context   default value is null and its the
+ *                          wrapper of the element we want to select if its considerd.
+ * @return {Node}           return the Element we select as s
+ */
     static select(s, context = null) {
       return (context) ? context.querySelector(s) : document.querySelector(s);
     }
-
+/**
+ * its the same as the select method but it can select multy Elements and return an array
+ * @param  {Node} s
+ * @param  {Node} context=null
+ * @return {Node}              return an array of the selected elements.
+ */
     static selectall(s, context = null) {
       // return Array.from(document.querySelectorAll(s));
       return (context) ? context.querySelectorAll(s) :  document.querySelectorAll(s) ;
     }
-
-    static on(parentSelector, eventType, elemSelector, fn) {
+/**
+ * making delegate event bye adding the event on the parent of an element.
+ * @TODO Select all of the element and loop for adding listeners in all elements.
+ * @param  {Node}   parent    the generic parent we want to add the listener on.
+ * @param  {Node}   eventType the type of the listener we receave to add line click or mousemove.
+ * @param  {Node}   elem      the element we want to add the delegate event on by adding it to its parrent.
+ * @param  {Function} fn      the function we receave with its e(Event) arg to add on the elem.
+ * @return {void}
+ */
+    static on(parent, eventType, elem, fn) {
       let func = (event) => {
-        if (typeof elemSelector === "string") {
-          if(event.target === Booking.$.select(elemSelector)) {
+        /**
+         * check if the elem passed is an object or a selector.
+         * @param  {any} typeofelem==="string"
+         * @return {voide}
+         */
+        if (typeof elem === "string") {
+          /**
+           * select the elem passed and run the function if its (clickd) on the target.
+           * @param  {Node} event.target===Booking.$.select(elem)
+           * @return {function}
+           */
+          if(event.target === Booking.$.select(elem)) {
             fn(event);
           }
         }else{
-          if(event.target === elemSelector) {
+          /**
+           * run the function if its (clickd) on the target.
+           * @param  {Node} event.target===elem
+           * @return {function}
+           */
+          if(event.target === elem) {
             fn(event);
           }
         }
       };
 
-      if (typeof parentSelector === "string") {
-        Booking.$.select(parentSelector).addEventListener(eventType, func);
+      if (typeof parent === "string") {
+        Booking.$.select(parent).addEventListener(eventType, func);
       } else {
-        parentSelector.addEventListener(eventType, func);
+        parent.addEventListener(eventType, func);
       }
     }
 
@@ -49,7 +83,7 @@ namespace Booking {
         const selectBox = Booking.$.select(selector);
         // console.log('its an elem');
         return selectBox.options[selectBox.selectedIndex].value;
-        
+
       }
     }
 
